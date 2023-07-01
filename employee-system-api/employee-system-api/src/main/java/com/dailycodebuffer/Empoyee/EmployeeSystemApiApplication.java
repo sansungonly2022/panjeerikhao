5d9f93eb658fc6822905442a9027a5e8318dc674
sansungonly2022 <sansungonly2022@gmail.com>
2023-07-01 12:10:26 -0400

    this is our first commit

---
+++ employee-system-api/employee-system-api/src/main/java/com/dailycodebuffer/Empoyee/EmployeeSystemApiApplication.java
@@ -0,0 +1,15 @@
+package com.dailycodebuffer.Empoyee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSystemApiApplication.class, args);
+        System.out.println("this is our first line of code");
+    }
+
+
+}
